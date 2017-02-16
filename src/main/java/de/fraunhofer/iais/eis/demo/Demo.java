077a9fa155d71ba2bfde0d62720ab9db5ca29634
christianmader <christian.mader@univie.ac.at>
2017-02-16 09:38:54 +0100

    updated and cleaned up demo class

--- src/main/java/de/fraunhofer/iais/eis/demo/Demo.java
+++ src/main/java/de/fraunhofer/iais/eis/demo/Demo.java
@@ -1,5 +1,6 @@
 package de.fraunhofer.iais.eis.demo;
 
+import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 import de.fraunhofer.iais.eis.*;
 import de.fraunhofer.iais.eis.util.ConstraintViolationException;
 import org.apache.jena.graph.impl.LiteralLabel;
@@ -12,26 +13,22 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.GregorianCalendar;
 
 /**
  * Created by christian on 08.02.17.
  */
 public class Demo {
 
-    public static void main(String[] args) {
-        try {
-            new Demo();
-        }
-        catch (ConstraintViolationException e) {
-            e.printStackTrace();
-        }
-        catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws ConstraintViolationException, MalformedURLException {
+        Demo demo = new Demo();
+        demo.createPolicy();
+        demo.createParameter();
+        demo.createBrokerRequests();
+        demo.createDataTransfer();
     }
 
-    public Demo() throws ConstraintViolationException, MalformedURLException {
-        /*
+    private void createPolicy() throws ConstraintViolationException {
         //todo: does not yet validate - data endpoint object can be obtained from backend system
         DataEndpoint permittedEndpoint = new DataEndpointBuilder().build();
 
@@ -41,17 +38,17 @@ public class Demo {
         Permission permission = new PermissionBuilder()
                 .targetDataEndpoint(permittedEndpoint)
                 .action(Arrays.asList(modifyAction))
-            .build();
+                .build();
 
         UsagePolicy policy = new UsagePolicyBuilder()
                 .permission(Arrays.asList(permission))
                 .prohibition(Collections.emptyList())
-            .build();
+                .build();
 
         System.out.println(policy.toRdf());
-        */
+    }
 
-        //-----
+    private void createParameter() throws MalformedURLException, ConstraintViolationException {
         Literal germanLabel = ResourceFactory.createLangLiteral("Stahlgüte", "de");
         Literal englishLabel = ResourceFactory.createLangLiteral("steel quality", "en");
 
@@ -62,25 +59,40 @@ public class Demo {
                 .semanticType(new URL("http://european-standards.org/manufactoring/steel#steelgrade"))
                 .paramMediaType(IANAMediaType.APPLICATION_CDMI_OBJECT)
                 .paramDescription(ResourceFactory.createLangLiteral("steel quality quality according to european standard", "en"))
-            .build();
+                .build();
         System.out.println(param.toRdf());
-
-        /*
-        Connector sourceConnector = new ConnectorBuilder().build();
-        Connector targetConnector = new ConnectorBuilder().build();
-
-        DataEndpoint sender = new DataEndpointBuilder().providedBy(sourceConnector).build();
-        DataEndpoint receiver = new DataEndpointBuilder().providedBy(targetConnector).build();
-
-        TransferredDataset builder = new TransferredDatasetBuilder()
-            .sender(sender)
-            .receiver(receiver)
-            .creationDate(new XMLGregorianCalendarImpl(new GregorianCalendar()))
-            .build();
-            */
     }
 
+    private void createBrokerRequests() throws ConstraintViolationException {
+        new BrokerQueryRequestBuilder()
+                .queryRequestAction(BrokerQueryRequestAction.GET)
+                .queryScope(BrokerQueryMessageScope.ACTIVE)
+                .messageContent("test")
+                .build();
+    }
 
+    private void createDataTransfer() throws ConstraintViolationException {
+        Participant dataOwner = new ParticipantBuilder()
+                .name(ResourceFactory.createPlainLiteral("Company A"))
+                .build();
+        Participant dataReceipient = new ParticipantBuilder()
+                .name(ResourceFactory.createPlainLiteral("Company B"))
+                .build();
 
+        Connector sourceConnector = new ConnectorBuilder()
+                .operatedBy(dataOwner)
+                .build();
+        Connector targetConnector = new ConnectorBuilder()
+                .operatedBy(dataReceipient)
+                .build();
 
+        DataEndpoint sender = new DataEndpointBuilder().publishedBy(sourceConnector).build();
+        DataEndpoint receiver = new DataEndpointBuilder().publishedBy(targetConnector).build();
+
+        DataTransfer transfer = new DataTransferBuilder()
+                .sender(sender)
+                .receiver(receiver)
+                .transferCreatedAt(new XMLGregorianCalendarImpl(new GregorianCalendar()))
+                .build();
+    }
 }
