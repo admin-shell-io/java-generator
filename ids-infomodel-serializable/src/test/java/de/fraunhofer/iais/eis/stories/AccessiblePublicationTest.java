5d745d5e42f99252aa0bd6f688b8844febe34efe
christianmader <christian.mader@univie.ac.at>
2018-05-08 14:48:59 +0200

    restructuring into modules

---
+++ ids-infomodel-serializable/src/test/java/de/fraunhofer/iais/eis/stories/AccessiblePublicationTest.java
@@ -0,0 +1,21 @@
+package de.fraunhofer.iais.eis.stories;
+
+import de.fraunhofer.iais.eis.util.ConstraintViolationException;
+import de.fraunhofer.iais.eis.util.VocabUtil;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import java.net.MalformedURLException;
+
+public class AccessiblePublicationTest {
+    /** As an IDS participant I want to make available xml data of the company's revenue development for retrieval via an HTTP-based interface */
+
+    @Ignore
+    @Test
+    public void createOffering() throws DatatypeConfigurationException, ConstraintViolationException, MalformedURLException {
+        // todo: apply protocol mapping capabilities of the IDS InfoModel
+        Assert.fail();
+    }
+}
