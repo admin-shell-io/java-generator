715b99cd1aad86b3e18fa9248580ce050670bc9b
christian <christian.mader@iais.fraunhofer.de>
2018-07-10 10:46:53 +0200

    add support for rdf resources

---
+++ java/src/main/java/de/fraunhofer/iais/eis/util/RdfResource.java
@@ -0,0 +1,14 @@
+package de.fraunhofer.iais.eis.util;
+
+public class RdfResource {
+
+    private String value;
+
+    public RdfResource(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
