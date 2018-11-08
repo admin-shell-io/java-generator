224e88653a58c40a571ab35eaee549f93013dd21
Benedikt Imbusch <benedikt.tobias.imbusch@iais.fraunhofer.de>
2018-11-08 10:13:50 +0100

    add first version of jsonld context generator

---
+++ jsonld-contextgen/src/main/java/de/fraunhofer/iais/eis/Generator.java
@@ -0,0 +1,75 @@
+package de.fraunhofer.iais.eis;
+
+import org.eclipse.rdf4j.model.BNode;
+import org.eclipse.rdf4j.model.Model;
+import org.eclipse.rdf4j.rio.RDFFormat;
+import org.eclipse.rdf4j.rio.Rio;
+
+import java.io.*;
+import java.util.*;
+
+public class Generator {
+
+    private static final Map<String,String> contextMap = new HashMap<>();
+
+    public static void main(String[] args) throws IOException {
+        if(args.length != 1) {
+            System.out.println("call with dest directory path");
+        } else {
+            File f  = new File(args[0]);
+            if(f.exists()) {
+                System.out.println("existing file is overwritten");
+                f.delete();
+                f.createNewFile();
+            }
+            PrintWriter printer = new PrintWriter(f);
+            printer.write(generate());
+            printer.flush();
+            printer.close();
+        }
+    }
+
+    public static String generate() throws IOException {
+        List<File> sourceTtl = getSourceTurtleFiles(new File("/home/benedikt/Documents/IDS/InformationModel/model/"));
+        sourceTtl.forEach(sourceFile -> {
+            try {
+                InputStream in = new FileInputStream(sourceFile);
+                System.out.println(sourceFile.getName());
+                Model model = Rio.parse(in, "/", RDFFormat.TURTLE);
+                model.getNamespaces().forEach(ns -> contextMap.put(ns.getPrefix(), ns.getName()));
+                model.subjects().stream().distinct()
+                        .filter(subj -> ! (subj instanceof BNode))
+                        .map(subj -> subj.stringValue())
+                        .forEach(subj -> {
+                            String simplePropertyOrClassName = subj.substring(subj.lastIndexOf("/") + 1); // after last slash
+                            Map.Entry<String, String> namespacePair = contextMap.entrySet().stream().filter(entry -> subj.contains(entry.getValue()))
+                                    .findAny().orElseThrow(() -> new UnsupportedOperationException("every property/class has to have a namespace" + subj));
+                            String prefixedName = subj.replace(namespacePair.getValue(), namespacePair.getKey() + ":");
+                            contextMap.put(simplePropertyOrClassName, prefixedName);
+                        });
+            } catch (IOException e) {
+                throw new RuntimeException(e); // hack to propagate exception
+            }
+        });
+        return buildContextString();
+    }
+
+    private static List<File> getSourceTurtleFiles(File root) {
+        List<File> sourceFiles = new ArrayList<>();
+        if(root.isFile() && root.getName().endsWith(".ttl")) {
+            sourceFiles.add(root); // recursion end
+        } else if(root.isDirectory() && !root.getAbsolutePath().contains("/references")) {
+            Arrays.stream(root.listFiles()).forEach(dir -> sourceFiles.addAll(getSourceTurtleFiles(dir))); // root.listFiles() should never be null
+        }
+
+        return sourceFiles; // includes case root == null
+    }
+
+    private static String buildContextString() {
+        final StringBuilder sbuilder = new StringBuilder();
+        sbuilder.append("{\r\n");
+        contextMap.entrySet().forEach(entry -> sbuilder.append("\t\"" + entry.getKey() + "\" : \"" + entry.getValue() + "\"\r\n"));
+        sbuilder.append("}\r\n");
+        return sbuilder.toString();
+    }
+}
