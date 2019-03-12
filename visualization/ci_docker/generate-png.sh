a3ff5a68928dd8837cb648c4de5e63022a248fa5
Benedikt Imbusch <benedikt.tobias.imbusch@iais.fraunhofer.de>
2019-03-12 16:54:39 +0100

    increase PLANTUML_LIMIT_SIZE to 16384
    this fixes png generation, as before parts of it were missing

--- visualization/ci_docker/generate-png.sh
+++ visualization/ci_docker/generate-png.sh
@@ -7,4 +7,5 @@ cd "$(dirname "$0")"
 cd ../../   # switch to ids-infomodel-codegen main dir
 mvn clean package -P CI -pl :visualization -s bamboo-config/maven-settings.xml
 cd visualization
+export PLANTUML_LIMIT_SIZE=16384
 java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar all_classes.plantuml
