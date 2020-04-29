4cfd834cb019bebc4323c107edccb81cba2bb443
mboeckmann <matthias.boeckmann@iais.fraunhofer.de>
2020-04-29 15:28:11 +0200

    Preparing for 3.1.0 release. Also improving RdfResource and making TypedLiteral a subclass of it

--- java/src/main/java/de/fraunhofer/iais/eis/util/RdfResource.java
+++ java/src/main/java/de/fraunhofer/iais/eis/util/RdfResource.java
@@ -1,19 +1,49 @@
 package de.fraunhofer.iais.eis.util;
 
-import com.fasterxml.jackson.annotation.JsonValue;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.io.Serializable;
+import java.net.URI;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RdfResource implements Serializable {
 
-    private String value;
+    @JsonProperty("@value")
+    @JsonAlias({"@value", "value"})
+    String value = null;
 
+    @JsonProperty("@type")
+    @JsonAlias({"@type", "type"})
+    String type = null;
+
+    public RdfResource() { super(); }
     public RdfResource(String value) {
         this.value = value;
     }
+    public RdfResource(String value, URI type) {
+        this.value = value;
+        this.type = type.toString();
+    }
 
-    @JsonValue
+    @JsonProperty("@value")
     public String getValue() {
         return value;
+    }
+
+    public void setValue(String value)
+    {
+        this.value = value;
+    }
+
+    @JsonProperty("@type")
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
     }
 }
